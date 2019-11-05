69f3b8b7b141e47438de6db8428b5ae91d5114f2
GreyZar37 <52458016+GreyZar37@users.noreply.github.com>
2019-11-05 18:57:19 +0100

    hu

---
+++ Abox/Assets/Scripts/GlobalBoxes.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class GlobalBoxes : MonoBehaviour
+{
+
+    public static int BoxCount;
+    public GameObject BoxDisplay;
+    public int InternalBox; 
+
+    void Update()
+    {
+        InternalBox = BoxCount; 
+        BoxDisplay.GetComponent<Text>().text = "Boxes: " + InternalBox; 
+    }
+}
---
+++ Abox/Assets/Scripts/Sell.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sell : MonoBehaviour
+{
+    public GameObject tekstbox;
+
+    public void ClickTheButton()
+    {
+        GlobalBoxes.BoxCount -= 1;
+        GlobalCash.CashCount += 1;
+    }
+
+
+}
