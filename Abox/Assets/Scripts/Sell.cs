396413674f967ac00088a63e098f51f0add55661
GreyZar37 <52458016+GreyZar37@users.noreply.github.com>
2019-11-19 10:12:35 +0100

    Sound effects

--- Abox/Assets/Scripts/Sell.cs
+++ Abox/Assets/Scripts/Sell.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class Sell : MonoBehaviour
 {
     public GameObject tekstbox;
-
+    public AudioSource sellSoundOne;
+    public AudioSource sellSoundTwo;
+    public int generateTone; 
     public void ClickTheButton()
     {
+        generateTone = Random.Range(1, 3);
         if (GlobalBoxes.BoxCount == 0)
         {
 
@@ -16,8 +19,20 @@ public class Sell : MonoBehaviour
         {
             GlobalBoxes.BoxCount -= 1;
             GlobalCash.CashCount += 1;
+            if (generateTone == 1)
+           
+            {
+                sellSoundOne.Play();
+
+            }
+            if (generateTone == 2)
+            {
+                sellSoundTwo.Play();
+
+            }
         }
-       
+
+
     }
 
 
