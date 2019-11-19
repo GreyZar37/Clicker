38fd70d89b7feaaf75bac54e5ff5df431b73b7df
GreyZar37 <52458016+GreyZar37@users.noreply.github.com>
2019-11-19 10:47:42 +0100

    More sounds

--- Abox/Assets/Scripts/Button.cs
+++ Abox/Assets/Scripts/Button.cs
@@ -6,12 +6,29 @@ public class Button : MonoBehaviour
 {
     public GameObject tekstBox;
     public Animator buttonanimation;
+    public AudioSource punchSoundOne;
+    public AudioSource punchSoundTwo;
+    public int generateTone;
 
 
     public void ClickTheButton()
     {
+        generateTone = Random.Range(1, 3);
+        if (generateTone == 1)
+
+        {
+            punchSoundOne.Play();
+
+        }
+        if (generateTone == 2)
+        {
+            punchSoundTwo.Play();
+
+        }
         GlobalBoxes.BoxCount += 1;
         buttonanimation.Play("PopUpBox");
+
+        
     }
 
 
--- Abox/Assets/Scripts/PurchaseLog.cs
+++ Abox/Assets/Scripts/PurchaseLog.cs
@@ -5,8 +5,30 @@ using UnityEngine;
 public class PurchaseLog : MonoBehaviour
 {
     public GameObject AutoBox;
+    public GameObject tekstbox;
+    public AudioSource sellSoundOne;
+    public AudioSource sellSoundTwo;
+    public int generateTone;
+
     public void StartAutoBox()
+
+
     {
+        generateTone = Random.Range(1, 3);
+        GlobalBoxes.BoxCount -= 1;
+        GlobalCash.CashCount += 1;
+        if (generateTone == 1)
+
+        {
+            sellSoundOne.Play();
+
+        }
+        if (generateTone == 2)
+        {
+            sellSoundTwo.Play();
+
+        }
+
         AutoBox.SetActive(true);
         GlobalCash.CashCount -= GlobalAutoClicker.autoValue;
         GlobalAutoClicker.autoValue *= 2;
